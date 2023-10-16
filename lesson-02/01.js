bde4f54bb3b8c5dd5b5d0c2d95157a34d4472889
Serzh-R <daarksen2@gmail.com>
2023-10-16 19:17:50 +0500

    change lesson-02.01

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,33 +1,3 @@
-// code
-
-// function showMessage2(val = 'Hellow!') {
-// 	console.log(val);
-// }
-// showMessage2();
-
-// function showMessage3(val = 'Hellow!') {
-// 	console.log(val);
-// }
-// showMessage3('By!');
-
-//-----------------------------------------
-
-// Стрелочная функция
-
-// const newSum = (a, b) => {
-// 	console.log(a + b);
-// };
-// newSum(3, 4);
-
-// const newSum2 = (a, b) => console.log(a + b);
-
-//--------------------------------------
-
-// function doArrow(value) {
-// 	console.log(value);
-// }
-// doArrow('Передай 5')
-
 const doArrow = (value) => console.log(value);
 
 doArrow('Передай 5');
