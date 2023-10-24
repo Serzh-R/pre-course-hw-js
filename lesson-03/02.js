bd30b3a43a24849064ee3fce8733dce234263742
Serzh-R <daarksen2@gmail.com>
2023-10-24 21:30:58 +0500

    added lesson-03.02

---
+++ lesson-03/02.js
@@ -0,0 +1,13 @@
+const words = ['яблоко', 'банан', 'апельсин', 'манго', 'киви'];
+
+if (words.indexOf('апельсин') >= 0) {
+	console.log('«Ура! нашел»');
+} else {
+	console.log('«Придется поискать в другом магазине…»');
+}
+
+// let result = words.indexOf('апельсин')
+// 	? console.log('«Ура! нашел»')
+// 	: console.log('«Придется поискать в другом магазине…»');
+
+// console.log(result);
